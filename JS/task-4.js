4cc400567b05bdfa76aeb57df1828ebcb38e9fca
Svetlana <ternovskaya.svetlana89@gmail.com>
2020-09-20 20:32:04 +0300

    Создает новые файлы

---
+++ JS/task-4.js
@@ -0,0 +1,37 @@
+class StringBuilder {
+  constructor(string) {
+    this._value = string;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  append(str) {
+    this._value += str;
+  }
+
+  prepend(str) {
+    this._value = str + this._value;
+  }
+
+  pad(str) {
+    this.append(str);
+    this.prepend(str);
+  }
+}
+
+console.log(typeof StringBuilder);
+// 'function'
+
+const builder = new StringBuilder('.');
+console.log(builder.value);
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
